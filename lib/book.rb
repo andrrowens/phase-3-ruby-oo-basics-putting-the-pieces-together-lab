008b7368a9728ee3863fb48810826fdbd76b0a90
Andrew Owens <andrrowens@gmail.com>
2023-01-10 11:44:10 -0700

    commit tree 9803dc6f943cdd5a939213411f02c4b9fc359534

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+require "pry"
 
+class Book
+
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+end
+
+new_book = Book.new('my new book')
+james_peach = Book.new('James and Giant Peach')
+puts james_peach.title
+puts new_book.title
+binding.pry
 
